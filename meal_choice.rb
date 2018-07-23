7decfe382524065efc0d4564ba1a6f2ac76ec8f7
nataliafb <nataliafigueroabernal@gmail.com>
2018-07-23 20:35:19 +0000

    Done.

--- meal_choice.rb
+++ meal_choice.rb
@@ -13,14 +13,6 @@ end
 dinner(food="salmon") 
   "Dinner is a gucci time for salmon!"
 end
-
-
-
-
-
-
-
-
 # Call the methods with puts and your own arguments here. Like this:
 puts snacks("Swedish fish")
 ## This returns "Any time, is the right time for Sweedish fish!"
