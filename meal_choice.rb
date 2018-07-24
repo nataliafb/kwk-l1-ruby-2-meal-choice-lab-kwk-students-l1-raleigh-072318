d41a959e11b91ff212e732430ac66703a7ed9358
nataliafb <nataliafigueroabernal@gmail.com>
2018-07-24 14:41:36 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -6,14 +6,13 @@ dinner = 'salmon'
 puts name + 'has this for breakfast' + 'and has this for lunch' +' this for dinner'
 
 puts 'whats the name for todays order?'
+name = gets
 
 puts 'what is the best thing to have for breakfast?'
-last_name = gets
+breakfast = gets
 
 puts ' what is the best thing to have for lunch?'
-birthyear = gets
+lunch = gets
 
 puts 'what is the best thing to have for dinenr?'
-birthplace = gets
-puts 'favorite color?'
-favcolor = gets 
+dinner = gets
