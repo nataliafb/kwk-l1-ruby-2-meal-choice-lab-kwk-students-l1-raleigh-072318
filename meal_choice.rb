bb7c8a85e9f6a4a520ba342cabcc0f1845244150
nataliafb <nataliafigueroabernal@gmail.com>
2018-07-24 14:36:09 +0000

    automatically backed up by learn

---
+++ meal_choice.rb
@@ -0,0 +1,16 @@
+breakfast = 'frosted flakes'
+lunch = 'grilled cheese'
+dinner = ''
+
+puts last_name + 'birth year is' + 'and is from' +' and fav color is '
+
+puts 'what is your last name?'
+last_name = gets
+
+puts ' what is your birth year?'
+birthyear = gets
+
+puts 'where were you born?'
+birthplace = gets
+puts 'favorite color?'
+favcolor = gets 
